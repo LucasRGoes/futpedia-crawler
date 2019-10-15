7d7f76ca79a7c679e29409343bc4609f89c98141
Lucas Góes <lucas.rd.goes@gmail.com>
2019-10-15 20:43:55 -0300

    Updating setup.py file with the markdown README.md file and the download_url variable.

--- setup.py
+++ setup.py
@@ -7,8 +7,10 @@ from setuptools import setup, find_packages
 MODULE_NAME = 'scrapedia'
 REPO_NAME = 'scrapedia'
 
+VERSION = open(join(MODULE_NAME, 'VERSION')).read().strip()
 
-with open('README.rst') as f:
+
+with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
@@ -21,11 +23,13 @@ setup(
                  ' historic data from the webpage futpedia.globo.com'),
     license=license,
     long_description=readme,
+    long_description_content_type='text/markdown',
     url='https://github.com/LucasRGoes/{:s}'.format(REPO_NAME),
+    download_url='https://github.com/LucasRGoes/{:s}/{:s}.tar.gz'.format(REPO_NAME, VERSION),
     author='Lucas Góes',
     author_email='lucas.rd.goes@gmail.com',
     packages=find_packages(exclude=('tests', 'docs')),
-    version=open(join(MODULE_NAME, 'VERSION')).read().strip(),
+    version=VERSION,
     install_requires=['beautifulsoup4==4.7.1', 'cachetools==3.1.1',
                       'pandas==0.24.2', 'requests==2.22.0',
                       'Unidecode==1.1.1'],
